d438c61d38de319a39d9a7f93c584c3286d8025e
azkuang <alexzkuang0314@gmail.com>
2023-11-23 10:55:00 -0500

    Gif Image testing

--- frontend/components/home/home.tsx
+++ frontend/components/home/home.tsx
@@ -1,11 +1,9 @@
 import styles from './home.module.css';
+import Image from 'next/image';
 
 export default function About() {
 	return (
         <section className={styles.container}>
-            <div className={styles.title}>
-                <h1>Health and Fitness Tracker</h1>
-            </div>
             <div className={styles.body}>
                 <p>
                     Fitness and Health Tracker is a Python-based application designed to 
@@ -13,6 +11,9 @@ export default function About() {
                     dietary habits, and other health metrics. The project leverages the 
                     Google Sheets API for easy data input and manipulation.
                 </p>
+            </div>
+            <div className={styles.image}>
+                <img src='/images/200w.gif' alt="Running Gif" />
             </div>
         </section>
     );
